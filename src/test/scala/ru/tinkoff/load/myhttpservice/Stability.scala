2f24886019800d050c642e26dfa17bc79fda6e7d
unknown <wolf781@yandex.ru>
2025-10-22 22:55:59 +0300

    commit3

--- src/test/scala/ru/tinkoff/load/myhttpservice/Stability.scala
+++ src/test/scala/ru/tinkoff/load/myhttpservice/Stability.scala
@@ -6,13 +6,17 @@ import ru.tinkoff.gatling.influxdb.Annotations
 import ru.tinkoff.load.myhttpservice.scenarios.CommonScenario
 
 class Stability extends Simulation with Annotations {
+  val rampDuration = 120
+  val stageDuration = 3600
+  val intensity = 16
+  val testDuration = 4000
 
   setUp(
     CommonScenario().inject(
       // Плавное увеличение нагрузки до целевого значения
-      rampUsersPerSec(0) to intensity.toInt during rampDuration,
+      rampUsersPerSec(0) to intensity during rampDuration,
       // Длительность полки
-      constantUsersPerSec(intensity.toInt) during stageDuration
+      constantUsersPerSec(intensity) during stageDuration
     )
   ).protocols(httpProtocol)
     // Общая длительность теста
